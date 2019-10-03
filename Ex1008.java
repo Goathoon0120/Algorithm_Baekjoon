e451343b68882899840a5eb87e8e059ae6cc45b7
LeeHoontae <dlgnsxo123@naver.com>
2019-10-03 23:26:56 +0900

    Add files via upload

---
+++ Ex1008.java
@@ -0,0 +1,16 @@
+package com.test.level1;
+
+import java.util.Scanner;
+
+public class Ex1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		
+		double ans = A / B;
+		
+		System.out.println(ans);
+	}
+}
